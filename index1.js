ff585c5826ec9394b4b75b7f83fc5289f95ebd7a
JasperShaw <b9014224@my.shu.ac.uk>
2021-02-15 17:21:48 +0000

    bar chart

---
+++ index1.js
@@ -0,0 +1,51 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+/**
+ *  App Configuration
+ */
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "javascripts")));
+
+
+
+/**
+ * Routes Definitions
+ */
+app.get("/", (req, res) => {
+    res.render("index", { title: "CatApp" });
+  });
+app.get("/studentinfo", (req, res) => {
+    res.render("studentinfo", { title: "studentinfo", studentProfile: { nickname: "TherapistName" } });
+  });  
+app.get("/filepond", (req, res) => {
+    res.render("filepond", { title: "filepond", filepondProfile: { nickname: "Filepond!" } });
+  });
+app.get("/login", (req, res) => {
+  res.render("login", { title: "login", loginProfile: { nickname: "Login!" } });
+});
+
+
+/**
+ * Server Activation
+ */
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+  });
